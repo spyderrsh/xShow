34b4ab5fdcad84b6d90342586c5e90e39d883422
spyderrsh <3034414+spyderrsh@users.noreply.github.com>
2024-07-09 22:38:03 -0600

    Push missing modified files

--- app/kvision/src/jsMain/kotlin/com/spyderrsh/xshow/ui/CommonStates.kt
+++ app/kvision/src/jsMain/kotlin/com/spyderrsh/xshow/ui/CommonStates.kt
@@ -2,8 +2,6 @@ package com.spyderrsh.xshow.ui
 
 import io.kvision.core.Container
 import io.kvision.html.div
-import io.kvision.html.p
-import io.kvision.html.span
 
 fun Container.ShowLoading() {
     div("Loading")
